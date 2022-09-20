3f60514bd92f4c5527765ec4640af600e5bbc62d
Rajesh kumar yadav <rajeshkumaryadav@Rajeshs-MacBook-Air.local>
2022-09-20 16:59:40 +0530

    I have fixed the code -Raj

--- prime.js
+++ prime.js
@@ -1,10 +1,13 @@
 function prime(num){
-  for(let i=0;i<=Math.sqrt(num);i++){
-    if(num%i==0){
-      return "not prime"
-    }
-    return "prime"
+  if(num<=1){
+    return "not prime";
   }
+  for(let i=2;i<=Math.sqrt(num);i++){
+    if(num%i==0){
+      return "not prime";
+    }
+  }
+  return "prime";
 }
 let data=prime(20);
-console.log(data)
+console.log(data);
